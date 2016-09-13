f9fce33b80024fe5e762603efa09443a7dded974
Stephanie Breunig <sbreunig@caltech.edu>
2016-09-13 14:30:12 -0700

    added numpy arrays practice

--- lesson20.py
+++ lesson20.py
@@ -14,3 +14,13 @@ def xa_to_diameter(xa):
     diameter = np.sqrt(xa * 4 / np.pi)
 
     return diameter
+
+A = np.array([[6.7, 1.3, 0.6, 0.7],
+              [0.1, 5.5, 0.4, 2.4],
+              [1.1, 0.8, 4.5, 1.7],
+              [0.0, 1.5, 3.4, 7.5]])
+
+B = np.array([1.1, 2.3, 3.3, 3.9])
+
+
+#print row 1 of A
