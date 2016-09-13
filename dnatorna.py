dcc9cab63791bbc587f54f46e03a771c4846afc7
Stephanie Breunig <sbreunig@caltech.edu>
2016-09-12 19:14:48 -0700

    Added the note that this is all upper case

--- dnatorna.py
+++ dnatorna.py
@@ -1,7 +1,7 @@
 """ convert DNA sequences to RNA """
 
 def rna(seq):
-    """ convert a DNA sequence to RNA"""
+    """ convert a DNA sequence to RNA, in all upper case"""
 
     #convert to uppercase
     seq = seq.upper()
