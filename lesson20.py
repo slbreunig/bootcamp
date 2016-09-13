84b1eb3429e71913d61a9af05e3360a1edb65dd7
Stephanie Breunig <sbreunig@caltech.edu>
2016-09-13 11:33:44 -0700

    updated to return diameter

--- lesson20.py
+++ lesson20.py
@@ -2,3 +2,15 @@ import numpy as np
 
 xa_high = np.loadtxt('data/xa_high_food.csv', comments = '#')
 xa_low = np.loadtxt('data/xa_low_food.csv', comments = '#')
+
+def xa_to_diameter(xa):
+    """
+    Convert an array of cross-sectional areas
+    to dimeters with commensurate units
+    """
+
+    #compute diameter from areas
+    #A = pi * d^2 / 4
+    diameter = np.sqrt(xa * 4 / np.pi)
+
+    return diameter
